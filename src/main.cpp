ad9073328bb0e4945f4cc3fd414dd486c2d9aba2
alice-thaitea <athai026@ucr.edu>
2021-05-20 23:54:30 -0700

    Wrote accompanying main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    int i = 1;
-    while (i < argc) {
-        cout << argv[i];
-        i++;
-    }
+    //int i = 1;
+    //while (i < argc) {
+    //    cout << argv[i];
+    //    i++;
+    //}
 
+    Factory* f = new Factory();
+    Base* output = f->parse(argv, argc);
+    if (output == nullptr) {
+        cout << "got nullptr" << endl;
+    }
+    else {
+        cout << "string: " << output->stringify() << endl;
+        cout << "answer: " << output->evaluate() << endl;
+    }
     cout << endl;
 
     return 0;
