12bde2ff2964f51f715cca8785a5e4df6a6183a9
alice-thaitea <athai026@ucr.edu>
2021-05-20 23:54:16 -0700

    Wrote factory parse function

--- src/factory.hpp
+++ src/factory.hpp
@@ -9,14 +9,76 @@
 #include "div.hpp"
 #include "pow.hpp"
 #include "rand.hpp"
+#include <iostream>
+#include <cctype>
+
+using namespace std;
 
 class Factory {
     public:
         Factory() {};
         Base* parse(char** input, int length) {
-            int i = 0;
+            int i = 1;
             Base* root = nullptr;
+            Base* left;
 
+            while (i < length) {
+                string in = static_cast<string>(input[i]);
+                string next = static_cast<string>(input[i+1]);
+                if (isdigit(in.at(0))) {
+                    left = new Op(stod(input[i]));
+                }
+                else if (in == "+") {
+                    if (isdigit(next.at(0))) {
+                        root = new Add(left, new Op(stod(input[++i])));
+                        left = root;
+                    }
+                    else {
+                        return nullptr;
+                    }
+                }
+                else if (in == "-") {
+                    if (isdigit(next.at(0))) {
+                        root = new Sub(left, new Op(stod(input[++i])));
+                        left = root;
+                    }
+                    else {
+                        return nullptr;
+                    }
+                }
+                else if (in == "*") {
+                    if (isdigit(next.at(0))) {
+                        root = new Mult(left, new Op(stod(input[++i])));
+                        left = root;
+                    }
+                    else {
+                        return nullptr;
+                    }
+                }
+                else if (in == "/") {
+                    if (isdigit(next.at(0))) {
+                        root = new Div(left, new Op(stod(input[++i])));
+                        left = root;
+                    }
+                    else {
+                        return nullptr;
+                    }
+                }
+                else if (in == "**") {
+                    if (isdigit(next.at(0))) {
+                        root = new Pow(left, new Op(stod(input[++i])));
+                        left = root;
+                    }
+                    else {
+                        return nullptr;
+                    }
+                }
+                else {
+                    cout << "Error" << endl;
+                    return nullptr;
+                }
+                i++;
+            }
             return root;
         }
 };
