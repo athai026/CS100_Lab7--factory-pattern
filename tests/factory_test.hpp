9e8a7652cd213afd6dcf2b96bbc843039fa9ef76
AEssaian740 <aessaian101@gmail.com>
2021-05-24 22:00:13 -0700

    Corrected include file names, and syntax error in value assignments

--- tests/factory_test.hpp
+++ tests/factory_test.hpp
@@ -3,30 +3,30 @@
 
 #include "gtest/gtest.h"
 #include "../src/op.hpp"
-#include "../src/Mult.hpp"
-#include "../src/Div.hpp"
-#include "../src/Add.hpp"
-#include "../src/Pow.hpp"
-#include "../src/Sub.hpp"
-#include "../src/Factory.hpp"
+#include "../src/mult.hpp"
+#include "../src/div.hpp"
+#include "../src/add.hpp"
+#include "../src/pow.hpp"
+#include "../src/sub.hpp"
+#include "../src/factory.hpp"
 
 TEST(FactoryTest, SimpleAdd) {
 	Factory test;
-	char* args[]=("3","+","4");
+	char* args[]={"3","+","4"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),7);
 }
 
 TEST(FactoryTest, SimpleSub) {
 	Factory test;
-	char* args[]=("7","-","4");
+	char* args[]={"7","-","4"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),3);
 }
 
 TEST(FactoryTest, SimpleDiv) {
 	Factory test;
-	char* args[]=("12","/","4");
+	char* args[]={"12","/","4"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),3);
 }
@@ -34,28 +34,28 @@ TEST(FactoryTest, SimpleDiv) {
 
 TEST(FactoryTest, SimpleMult) {
 	Factory test;
-	char* args[]=("3","*","4");
+	char* args[]={"3","*","4"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),12);
 }
 
 TEST(FactoryTest, SimplePow) {
 	Factory test;
-	char* args[]=("3","**","2");
+	char* args[]={"3","**","2"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),9);
 }
 
 TEST(FactoryTest, AddSub) {
 	Factory test;
-	char* args[]=("3","+","4","-","5");
+	char* args[]={"3","+","4","-","5"};
 	Base* op=test.parse(args,5);
 	EXPECT_DOUBLE_EQ(op->evaluate(),2);
 }
 
 TEST(FactoryTest, MultAdd) {
 	Factory test;
-	char* args[]=("3","*","4","+","2");
+	char* args[]={"3","*","4","+","2"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),14);
 }
@@ -63,7 +63,7 @@ TEST(FactoryTest, MultAdd) {
 
 TEST(FactoryTest, MultDiv) {
 	Factory test;
-	char* args[]=("3","*","4","/","3");
+	char* args[]={"3","*","4","/","3"};
 	Base* op=test.parse(args,3);
 	EXPECT_DOUBLE_EQ(op->evaluate(),4);
 }
